3057a2f52ae42b19464081124b08432855dd45bf
Nileshlad <60500269+Nileshlad@users.noreply.github.com>
2020-05-17 04:13:07 +0530

    Added: How To Sort ArrayList In Descending Order In Java

---
+++ src/main/java/com/list/sortingarraylist/ArrayListDescendingSort2.java
@@ -0,0 +1,31 @@
+package com.list.sortingarraylist;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayListDescendingSort2 {
+    public static void main(String args[]) {
+
+        ArrayList<Integer> arraylist = new ArrayList<Integer>();
+        arraylist.add(1);
+        arraylist.add(13);
+        arraylist.add(89);
+        arraylist.add(45);
+
+        /*Unsorted List: ArrayList content before sorting*/
+        System.out.println("ArrayList Before Sorting:");
+        for (int num : arraylist) {
+            System.out.println(num);
+        }
+
+        /* Sorting in decreasing (descending) order*/
+        Collections.sort(arraylist, Collections.reverseOrder());
+
+        /* Sorted List in reverse order*/
+        System.out.println("ArrayList in descending order:");
+        for (int num : arraylist) {
+            System.out.println(num);
+        }
+    }
+}
+
