a9d1dcacf929987c34b9303ddd3aadad492e9da2
Nileshlad <60500269+Nileshlad@users.noreply.github.com>
2020-05-17 03:25:59 +0530

    Added: a program to get size of HashMap

--- src/main/java/com/map/HashMapSizeExample.java
+++ src/main/java/com/map/HashMapSizeExample.java
@@ -10,11 +10,11 @@ public class HashMapSizeExample {
         HashMap<Integer, String> hashmap = new HashMap<Integer, String>();
 
         // Adding Key and Value pairs to HashMap
-        hashmap.put(11,"Apple");
-        hashmap.put(22,"Banana");
-        hashmap.put(33,"Mango");
-        hashmap.put(44,"Pear");
-        hashmap.put(55,"PineApple");
+        hashmap.put(11, "Apple");
+        hashmap.put(22, "Banana");
+        hashmap.put(33, "Mango");
+        hashmap.put(44, "Pear");
+        hashmap.put(55, "PineApple");
 
         // int size() method returns the number of key value pairs
         System.out.println("Size of HashMap : " + hashmap.size());
