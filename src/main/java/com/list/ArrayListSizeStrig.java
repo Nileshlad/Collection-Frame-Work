bc8dd599fc6ab2390d8f0cd9e223e10e84a6b6df
Nileshlad <60500269+Nileshlad@users.noreply.github.com>
2020-05-17 03:21:28 +0530

    Added: a program to add elements to HashSet

---
+++ src/main/java/com/list/ArrayListSizeStrig.java
@@ -0,0 +1,34 @@
+/* Program to find size of ArrayList in Java */
+
+package com.list;
+
+import java.util.ArrayList;
+
+class ArrayListSizeString{
+    public static void main(String[] args) {
+        System.out.println("Java Program to find the size of ArrayList");
+
+        // Create an String ArrayList Object
+        ArrayList<String> listOfCities = new ArrayList<>();
+
+        int size = listOfCities.size();
+        // Print initial size of ArrayList
+        System.out.println("size of ArrayList after creation: " + size);
+
+        // Adding elements to ArrayList Object
+        listOfCities.add("California");
+        listOfCities.add("Boston");
+        listOfCities.add("New York");
+
+        size = listOfCities.size();
+       /* Print size of ArrayList
+          after adding elements */
+        System.out.println("size of ArrayList after adding elements: " + size);
+
+        // clear() method removes all elements
+        listOfCities.clear();
+
+        size = listOfCities.size();
+        System.out.println("size of ArrayList after clearing elements: " + size);
+    }
+}
